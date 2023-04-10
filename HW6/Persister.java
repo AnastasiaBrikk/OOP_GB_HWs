292ae3b1f9b6deeb7996ba899c8511ea803036c2
Anastasia <115826673+AnastasiaBrikk@users.noreply.github.com>
2023-04-10 17:48:41 +0300

    HomeWork6

---
+++ HW6/Persister.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Persister<T> extends ArrayList<T>{
+    private Persister<T> users; // типа база данных в листе
+
+    public Persister(){
+        this.users = null;
+    }
+
+}
---
+++ HW6/SaveUser.java
@@ -0,0 +1,7 @@
+public class SaveUser {
+
+    public static void save(User user) { // можно добавить E db для сохранения в него data
+        // db.add(data); сохранение в лист данных.
+        System.out.println("Save user: " + user.getName());
+    }
+}
